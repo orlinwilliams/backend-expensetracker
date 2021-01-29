8fd36b68658106fd638a95cb1d82a34910e27796
orlinwilliams <orlinwilliams0077@gmail.com>
2021-01-28 19:12:30 -0600

    update endpoint category-expense

--- src/controllers/categories/expense-controller.js
+++ src/controllers/categories/expense-controller.js
@@ -68,9 +68,81 @@ categories.updateAnExpense = async (req, res) => {
         },
       }
     );
+
+    //found expenses to update
+    const expensesFoundToUpdate = await expensesFound(
+      req.params.idExpenseCategory
+    );
+    console.log(expensesFoundToUpdate);
+    //updating
+    updateExpenseFound(
+      expensesFoundToUpdate,
+      req.params.idUser,
+      req.body.title,
+      req.body.limit
+    );
+
     res.status(201).json({ error: false, data: expense });
   } catch (error) {
     res.status(400).json({ error });
+  }
+};
+const expensesFound = async (idCategoryExpense) => {
+  try {
+    //training find
+    const searchExpenses = await User.aggregate([
+      {
+        $match: {
+          'expenses.category._id': idCategoryExpense,
+        },
+      },
+      {
+        $unwind: '$expenses',
+      },
+      {
+        $match: {
+          'expenses.category._id': idCategoryExpense,
+        },
+      },
+      {
+        $project: {
+          _idExpense: '$expenses._id',
+          value: '$expenses.value',
+          category: '$expenses.category',
+          date: '$expenses.date',
+        },
+      },
+    ]);
+    if (searchExpenses) {
+      return searchExpenses;
+    }
+  } catch (error) {
+    console.log(error);
+  }
+};
+const updateExpenseFound = async (
+  expensesFound,
+  idUser,
+  newTitle,
+  NewLimit
+) => {
+  try {
+    expensesFound.forEach(async (item) => {
+      const updateAnExense = await User.updateOne(
+        {
+          _id: idUser,
+          'expenses._id': mongoose.Types.ObjectId(item._idExpense),
+        },
+        {
+          $set: {
+            'expenses.$.category.title': newTitle,
+            'expenses.$.category.limit': NewLimit,
+          },
+        }
+      );
+    });
+  } catch (error) {
+    console.log(error);
   }
 };
 
